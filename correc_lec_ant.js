03e1b9e82e8d14b6bf6ec59be3082124edd8554b
Socos314 <Juandacasdiaz@gmail.com>
2019-09-05 23:16:24 -0500

    Actualizacion

--- correc_lec_ant.js
+++ correc_lec_ant.js
@@ -100,29 +100,11 @@ $(document).ready(function() {
                 
             }
 			$("#co_leer").html("<span class='glyphicon glyphicon-floppy-disk'></span> Actualizar");
-			$("#co_cancelar").html("<span class='glyphicon glyphicon-log-out'></span> Cancelar");
+			$("#co_cancelar").html("<span class='glyphicon glyphicon-log-out'></span> Limpiar");
 			//fn_carga_orden();
           
            
 		}
-			////else{
-			
-				////if( $("#cb_reasigna_nuevo").val() == ""){
-				////	fn_mensaje_boostrap("SELECCIONE EL USUARIO PARA REALIZAR LA ASIGNACIÓN", g_titulo, $("#cb_reasigna_nuevo"));
-					////return;
-				////}
-	////
-				////if($("#tx_rol_actual").val() == $("#cb_reasigna_nuevo").val())
-				////{
-				////	fn_mensaje_boostrap("DEBE SELECCIONAR UN USUARIO DIFERENTE AL ACTUAL", g_titulo, $("#cb_reasigna_nuevo"));
-					////return;
-			////	}
-			//////////////////////////////////////////////////////////////
-			/////////////////SE ACTUALIZA EL REGISTRO/////////////////////
-			//////////////////////////////////////////////////////////////
-				////fn_act_orden();
-				////return;	
-			////}
 	});
     
     
@@ -135,7 +117,7 @@ $(document).ready(function() {
 		 });
 
 	$("#co_cancelar").on("click",function(){
-		if ($.trim($("#co_cancelar").text())=="Cancelar"){
+		if ($.trim($("#co_cancelar").text())=="Limpiar"){
 			fn_limpiar();
 			return;
 		}
@@ -169,64 +151,9 @@ $(document).ready(function() {
 function fn_setea_grid_principal()
 { 
     var data = [
-        { c1:8000, c2:2000, c3: 'MCUB', c4:2014, c5:97985,c6:1000, c7: 0,c8:89,c9:30},
-    { c1:0, c2:0, c3: '', c4:0, c5:0,c6:0, c7: 0,c8:0,c9:0},
+    { c1:'RCARVAJAL', c2:'23455', c3: '01/03/2019', c4:'CORECCION', c5:97985,c6:7985, c7: 'SE CAMBIA LECTURA'},
     ];
-var obj = {
-            width: '100%',
-            height: 200,
-            showTop: true,
-			showBottom:false,
-            showHeader: true,
-            roundCorners: true,
-            rowBorders: true,
-            columnBorders: true,
-			editable:true,
-            selectionModel: { type: 'cell' },
-            numberCell: { show: false },
-            title: "Auditoria de Modificaciones",
-			pageModel: {type:"local"},
-        	scrollModel:{autoFit:true, theme:true},
-            toolbar:
-        {
-            cls: "pq-toolbar-export",
-            items:
-            [
-				{ type: "button", label: "Excel", attr:"id=co_excel2", cls:"btn btn-primary btn-sm"},
-				
-            ]
-        }
-        };
-		
-		
-		obj.colModel = [
-            { title: "Numero medidor",  resizable: false, width: 80, dataType: "number", dataIndx: "c1",halign:"center", align:"right",editable:false},
-            { title: "Enteros Decimales", width: 80, dataType: "number", dataIndx: "c2",halign:"center", align:"right",editable:false},
-            { title: "Tipo Medida", width: 80, dataType: "string", dataIndx: "c3",halign:"center", align:"right",editable:false},
-            { title: "Lectura Anterior", width: 80, dataType: "number", dataIndx: "c4",halign:"center", align:"right" },
-            { title: "Lectura Actual", width: 80, dataType: "number", dataIndx: "c4",halign:"center", align:"right",editable:false },
-            { title: "Consumo", width: 80, dataType: "number", dataIndx: "c5",halign:"center", align:"right",editable:false},
-            { title: "CONSUMO A FACTURAR GLS",width: 80, dataType: "number", dataIndx: "c6",halign:"center", align:"right",editable:false},
-            { title: "Factor Conversión Consumo",width: 80, dataType: "number", dataIndx: "c7",halign:"center", align:"right",editable:false},
-            { title: "Periodo Dias Promediados",width: 80, dataType: "number", dataIndx: "c8",halign:"center", align:"right",editable:false},
-             { title: "Periodo Dias Normalizados",width: 80, dataType: "number", dataIndx: "c9",halign:"center", align:"right",editable:false}
-             
-             //align: "center", editable: false, minWidth: 100, sortable: false,
-				
-             //render: function (ui) {
-				//	return "<button class='btn btn-primary btn-sm'>Eliminar</button>";
-				//}
-			//}
-        ];
-		
-		obj.dataModel = { data: data };
-
-var grid = pq.grid("#div_grid", obj);
-
-    var data = [
-        { c1:'RCARVAJAL', c2:'23455', c3: '01/03/2019', c4:'CORECCION', c5:97985,c6:7985, c7: 'SE CAMBIA LECTURA'},
-    ];
-var obj = {
+    var obj = {
             width: '100%',
             height: 200,
             showTop: true,
@@ -327,7 +254,7 @@ function fn_leer(){
 	if ($.trim($("#co_leer").text()) == "Leer")
 	{
 				
-		$("#tx_orden").val("");
+		
 		$("#tx_cod_cliente").val("45223");
 		$("#tx_dir").val("Maria");
 		$("#tx_est_client").val("Rol-1");
@@ -337,7 +264,7 @@ function fn_leer(){
 		$("#tx_tarif").val("Activo");
 		$("#tx_actividad").val("1");
 		$("#tx_num_medidor").val("23");            
-		$("#tx_ent-decim").val("23"); 
+		$("#tx_ent_decim").val("23"); 
 		$("#tx_tipo_med").val("Tipo_1");  
 		$("#tx_lec_ant").val("23444"); 
 
@@ -354,13 +281,14 @@ function fn_leer(){
 		$("#tx_peri_dia_prom").val("2"); 
 		$("#tx_peri_dia_norm ").val("2"); 
 		$("#tx_num_medidor2").val("");         
-		$("#tx_ent-decim2").val(""); 
+		$("#tx_ent_decim2").val(""); 
 		$("#tx_tipo_med2").val(""); 
-		$("#tx_lec-ant2").val(""); 
-		$("#tx_lec-actu2").val(""); 
+		
+		$("#tx_lec_actu2").val(""); 
 		$("#tx_consum2").val(""); 
 		
-		$("#tx_lec_ant").prop("disabled", false);  //Para habilitar
+		$("#tx_lec_ant").prop("disabled", false);
+        $("#tx_lec_ant2").prop("disabled", false); //Para habilitar
 
 	}
 }
@@ -403,22 +331,42 @@ function fn_act_orden(){
 }
 
 function fn_limpiar(){
-	$("#tx_orden").val("");
-	$("#tx_cod_cliente").val("");
-	$("#tx_nombre").val("");
-	$("#tx_rol_actual").val("");
-	$('#cb_reasigna_nuevo').val("");
-	$("#tx_tarifa").val("");
-	$("#tx_actividad").val("");
-	$("#tx_estado").val("");
-	$("#tx_ruta").val("");
+	
+		$("#tx_cod_cliente").val("");
+		$("#tx_dir").val("");
+		$("#tx_est_client").val("");
+		$('#tx_est_conex').val("");
+		$("#tx_reg").val("");
+		$("#tx_ruta").val("");
+		$("#tx_tarif").val("");
+		$("#tx_actividad").val("");
+		$("#tx_num_medidor").val("");            
+		$("#tx_ent_decim").val(""); 
+		$("#tx_tipo_med").val("");  
+		$("#tx_lec_ant").val(""); 
 
-	$("#tx_orden").prop("disabled",false);
-	$("#co_leer").html("<span class='glyphicon glyphicon-search'></span> Leer");
-	$("#co_leer").prop("disabled",false);
-	$("#co_cancelar").html("<span class='glyphicon glyphicon-off'></span> Cerrar");
-	//$("#co_reasignar").prop("disabled",true);
-	$("#cb_reasigna_nuevo").prop("disabled",true);
+		$("#tx_lec-actu").val("");  
+		$("#tx_consum").val(""); 
+		$("#tx_regional").val("");  
+		$("#tx_ruta").val("");  
+		$("#tx_tarifa").val("");  
+		$("#tx_regional").val("");  
+		$("#tx_ruta").val("");  
+		$("#tx_tarifa").val("");  
+		$("#tx_fac_conv_consum").val("");  
+		$("#tx_consum_fact_gls").val("");  
+		$("#tx_peri_dia_prom").val(""); 
+		$("#tx_peri_dia_norm ").val(""); 
+		$("#tx_num_medidor2").val("");         
+		$("#tx_ent_decim2").val(""); 
+		$("#tx_tipo_med2").val(""); 
+		
+		$("#tx_lec_actu2").val(""); 
+		$("#tx_consum2").val(""); 
+		
+		$("#tx_lec_ant").prop("disabled", false);
+        $("#tx_lec_ant2").prop("disabled", false); //Para habilitar
+$("#tx_orden").val("");
 	$("#tx_orden").focus();
 }
 	
