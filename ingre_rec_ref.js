694e28c3f58335654497a4c8c216899bc4f6fb4c
Juan David Castrillon Diaz <Juandacasdiaz@gmail.com>
2019-09-20 16:05:55 -0500

    Revert "Merge branch 'master' of https://github.com/jotaqqta/local_html"
    This reverts commit cbb0dc54b0e8480b01b192fb1182bfc88000f03c, reversing
    changes made to d8134b35fdaa2807317b5fa281d374626d1617be.

--- ingre_rec_ref.js
+++ ingre_rec_ref.js
@@ -2,7 +2,7 @@ var g_modulo="Facturación Clientes - Lecturas y Consumos";
 var g_titulo="Ingreso Requerimientos Refacturados.";
 var parameters={};
 var my_url="correc_prome_dudo.asp";
-var $grid, $grid_tar_izq;
+var $grid;
 //~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
 $(document).keydown(function(e) {
 
@@ -19,12 +19,7 @@ $(document).ready(function() {
     $("#co_obs").prop("disabled", true);
 
 	$("#cb_motiv").prop("disabled",true);
-	$("#comuna").hide();
-	$("#new_file").hide();
-	$("#panel_tarifas").hide();
-	$("#grid_secundaria").hide();
-    $("#boton_secun").hide();
-
+    
     fn_tip_ajust();
     fn_origen();
 	
@@ -83,13 +78,8 @@ $(document).ready(function() {
 	});
 
     $("#co_aceptar").on("click", function () {
-		//////////////////////////////////////////////////////////////////////////////////////////
-		//===========================OJO SE DEBE QUITAR ESTE COMENTARIO=========================//
-		//=============================SE HIZO PARA HACER LAS PRUEBAS===========================//
-		//////////////////////////////////////////////////////////////////////////////////////////
-		
+		//Validación de informacion
 		if ($.trim($("#co_aceptar").text()) == "Aceptar") {
-
 			if ($("#cb_tip_ajust").val() ==""){
 				fn_mensaje_boostrap("SELECCIONE TIPO DE AJUSTE", g_titulo, $("#cb_tip_ajust"));
                 return;
@@ -104,16 +94,7 @@ $(document).ready(function() {
                 return;    
             }
                 }
-        }      
-    	
-        var vOrigen = $("#cb_origen").val();
-		
-		$("#tx_comuna").val("PEDREGAL(PMA)");
-		$("#tx_clave").val("01");
-		$("#tx_ajuste").val("002");
-		$("#tx_origen").val(vOrigen);
-
-        fn_ventana_final();
+        }
 	});
 
     $("#co_cancelar").on("click",function(){
@@ -155,7 +136,7 @@ $(document).ready(function() {
 function fn_setea_grid_principal()
 { 
     var data = [
-		{ C1:'69792375', C2: "14/03/2078", C3:'FA', C4:'16.2', C5:'No Refacturado', C6:'No', C7:'No', C8:'03-2018'},
+		{ C10:false, C1:'69792375', C2: "14/03/2078", C3:'FA', C4:'16.2', C5:'No Refacturado', C6:'No', C7:'No', C8:'03-2018'},
         	{ C1:'69334507', C2: "14/02/2018", C3:'FA', C4:'15.2', C5:'No Refacturado', C6:'No', C7:'No', C8:'02-2018'},
         	{ C1:'68848888', C2: "15/01/2018", C3:'FA', C4:'7.36', C5:'No Refacturado', C6:'No', C7:'No', C8:'01-2018'},
         	{ C1:'68929696', C2: "14/12/2017", C3:'FA', C4:'7.36', C5:'No Refacturado', C6:'No', C7:'No', C8:'12-2018'}
@@ -163,7 +144,7 @@ function fn_setea_grid_principal()
     ];
     var obj = {
             width: '100%',
-            height: 250,
+            height: 400,
             showTop: true,
 			showBottom:false,
             showHeader: true,
@@ -180,19 +161,10 @@ function fn_setea_grid_principal()
         };
 		obj.colModel = [
             
-            { dataIndx: "", width: "10%", align: "center", resizable: false,
-                type: "checkbox", cls: "ui-state-default", sortable: false, editor: false, editable: true,
-                dataType: "bool",
-                cb: {
-                    all: false,
-                    header: true,
-                    select: true,
-                    all: true
-                }
-            },
+            { type: 'checkbox',title: "", width: 5, dataType: "string", dataIndx: "C10",halign:"center", align:"center" },
             { title: "Documento",  resizable: false, width: 100, dataType: "number", dataIndx: "C1",halign:"center", align:"right"},
             { title: "Fecha", width: 100, dataType: "string", dataIndx: "C2",halign:"center", align:"center" },
-            //{ title: "valor_chk", width: 80, dataType: "string", dataIndx: "C10",halign:"center", align:"center", hidden:true },
+            { title: "valor_chk", width: 80, dataType: "string", dataIndx: "C10",halign:"center", align:"center", hidden:true },
             { title: "Tipo", width: 90, dataType: "number", dataIndx: "C3",halign:"center", align:"right" },
             { title: "Valor", width: 90, dataType: "number", dataIndx: "C4",halign:"center", align:"right" },
             { title: "Refacturado",width: 140, dataType: "number", dataIndx: "C5",halign:"center", align:"right"},
@@ -207,132 +179,6 @@ function fn_setea_grid_principal()
 		$grid.pqGrid( "refreshDataAndView" );
 
 }
-
-//~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*	
-function fn_setea_grid_secundaria()
-{ 
-    var data = [
-		{C1:'69792375', C2: "FA", C3:'03-2018', C4:'16.2', C5:'ES. PMÁ - COLON ALCANTARILLADO', C6:'No', C7:'No', C8:'03-2018'}	
-    ];
-    var obj = {
-            width: '75%',
-            height: 170,
-            showTop: true,
-			showBottom:false,
-            showHeader: true,
-            roundCorners: true,
-            rowBorders: true,
-            columnBorders: true,
-			editable:true,
-            selectionModel: { type: 'cell' },
-            numberCell: { show: false },
-            title: "Titulo",
-			pageModel: {type:"local"},
-        	scrollModel:{theme:true},
-        
-    };
-	obj.colModel = [
-        
-        { title: "Documento",  resizable: false, width: 100, dataType: "number", dataIndx: "C1",halign:"center", align:"right"},
-        { title: "Tipo", width: 80, dataType: "number", dataIndx: "C2",halign:"center", align:"center" },
-        { title: "Fecha", width: 100, dataType: "string", dataIndx: "C3",halign:"center", align:"center" },
-        { title: "Valor", width: 80, dataType: "number", dataIndx: "C4",halign:"center", align:"center" },
-        { title: "Tarifa", width: 403, dataType: "string", dataIndx: "C5",halign:"center", align:"left" },
-        { title: "Ajustado", width: 90, dataType: "string", dataIndx: "C6",halign:"center", align:"center" }
-	];
-
-	obj.dataModel = { data: data };
-
-	$grid = $("#div_grid_secun").pqGrid(obj);
-	$grid.pqGrid( "refreshDataAndView" );
-}
-
-//~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*	
-function fn_setea_grid_tarifa()
-{    	
-    var obj_tar_izq = {
-        width:"100%",
-		minWidth:10,
-        height:250,
-        rowBorders:true,
-		fillHandle: "",
-        numberCell: { show: false },
-        collapsible: { on : false,toggle:false },
-        stripeRows : true,
-        pasteModel: { on: false },
-        showBottom: false,
-        showTop: false,
-        swipeModel: { on: false },
-        colModel:
-        [
-            { dataIndx: "", width: "10%", align: "center", resizable: false,
-                //type: "checkbox", 
-                cls: "ui-state-default", sortable: false, editor: false, editable: true,
-                dataType: "bool",
-                cb: {
-                    all: false,
-                    header: true,
-                    select: true,
-                    all: true
-                }
-            },
-            { title: "Codigo", width: 0, align: "center", dataIndx:"C1",hidden:true },
-            { title: "CARGOS ORIGINALES", width: "87%", align: "left", dataIndx:"C2", halign:"center", align:"left", editable: false,
-				filter: { type: "textbox", condition: "contain", listeners: ["keyup"]}}
-        ],
-        selectionModel: { type: "row" }
-    };	
-
-    $grid_tar_izq = $("#grid_tar_izq").pqGrid(obj_tar_izq);
-    $grid_tar_izq.pqGrid( "option", "dataModel.data", [] );
-
-    var obj_tar_der = {
-        width:"100%",
-        height:250,
-		minWidth:10,
-        rowBorders:true,
-		fillHandle: "",
-        numberCell: { show: false },
-        collapsible: { on : false,toggle:false },
-        stripeRows : true,
-        pasteModel: { on: false },
-        showBottom: false,
-        showTop: false,
-        swipeModel: { on: false },
-        colModel:
-        [
-            { dataIndx: "", width: "10%", align: "center", resizable: false,
-                //type: "checkbox", 
-                cls: "ui-state-default", sortable: false, editor: false, editable: true,
-                dataType: "bool",
-                cb: {
-                    all: false,
-                    header: true,
-                    select: true,
-                    all: true
-                }
-            },
-            { title: "Codigo", width: 0, dataIndx:"C1", hidden:true},
-            { title: "CARGOS NUEVOS", width: "87%", dataIndx:"C2", halign:"center", align:"left", editable: false,
-                render: function (ui) {
-                    var rowData = ui.rowData;
-
-                    var farorojo = tarifa_regis.indexOf(rowData.C1);
-
-                    if(farorojo==-1)
-                    {
-                        tarifa_regis.push(rowData.C1);
-                    }
-                } 
-            }
-        ],
-        selectionModel: { type: "row" }
-    };	
-		
-    $grid_tar_der = $("#grid_tar_der").pqGrid(obj_tar_der);
-    $grid_tar_der.pqGrid( "option", "dataModel.data", [] );	
-}
-//~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*	
 
 function fn_leer(){
 	var f = new Date();
@@ -374,10 +220,6 @@ function fn_limpiar(){
 	$("#tx_actividad").val("");
     $("#tx_refe").val("");
     $("#co_obs").val("");
-    $("#tx_comuna").val("");
-	$("#tx_clave").val("");
-	$("#tx_ajuste").val("");
-	$("#tx_origen").val("");    
 
 	$("#tx_cliente").val("");
 	$("#tx_cliente").prop("disabled", false);	
@@ -400,29 +242,6 @@ function fn_Muestra_ingre() {
 }
 
 //*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
-function fn_ventana_final(){
-	$('#div_ing_bts').modal('hide');
-	$("#co_leer").hide();
-	$("#co_cancelar").hide();
-	$("#direccion").hide();
-	$("#comuna").show();
-	$("#fila3").hide();
-	$("#fila4").hide();
-	$("#fila5").hide();
-	$("#fila6").hide();
-	$("#fila7").hide();
-	$("#new_file").show();
-	
-	$("#panel_tarifas").show();
-	$("#grid_secundaria").show();
-	$("#boton_secun").show();
-	
-	$("#co_aprobar").prop("disabled", true);
-	fn_setea_grid_secundaria();
-	fn_setea_grid_tarifa();
-
-}
-//*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
 /////////////////////////////////FUNCIONES MODAL///////////////////////////////////////////
 function fn_tip_ajust() {
 
@@ -439,7 +258,7 @@ function fn_motiv() {
 //*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
 function fn_origen() {
 
-	$("#cb_origen").html("<option value='' selected></option><option value='005'>005</option> <option value='010' >010</option> <option value='015'>015</option>");
+	$("#cb_origen").html("<option value='' selected></option><option value='1'>005</option> <option value='2' >010</option> <option value='3'>015</option>");
 }
 
 //*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
