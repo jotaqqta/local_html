4f1863964a43215dba55d33c97beba60d9693b61
Juan David Castrillon Diaz <Juandacasdiaz@gmail.com>
2019-10-02 10:43:31 -0500

    Actualización admin_cargos

--- admin_cargos.js
+++ admin_cargos.js
@@ -1,15 +1,10 @@
 var g_modulo = "Administración Central - Configuración Base del Sistema";
 var g_tit = "Consulta de cargos";
-
 var $grid_principal;
 var $grid_2;
-var $grid_3;
-
 var sql_grid_prim = "";
-var sql_grid_2    = "";
-var sql_grid_3    = "";
-
-
+var sql_grid_2 = "";
+var sql_grid_3 = "";
 var parameters = {};
 
 
@@ -41,7 +36,6 @@ $(document).ready(function () {
 	
 	//Footer  ///raiz/
 	$("#div_footer").load("syn_globales/footer.htm");
-	
 	$("#excel_archivo").val("tablas_generales.xls");
     $("#tx_empresa").val("1");
 	$("#tx_rol").val("SYNERGIA");
@@ -59,29 +53,17 @@ $(document).ready(function () {
     $("#co_sig").html("<span class='glyphicon glyphicon-arrow-right'></span> Siguiente");
 	$("#co_selec").html("<span class='glyphicon glyphicon-plus'></span> Seleccionar");
     $("#co_cerrar").html("<span class='glyphicon glyphicon-off'></span> Cerrar");
-	
-//~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*	
-//FUNCIONES DE CAMPOS
-
-
 //~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*	
 //BOTONES-EVENTOS
-   $("#co_leer").on("click", fn_Muestra_Filtro);	
-
-	
-    $("#co_editar").on("click", function (e) {
-		fn_modal(2);
-	});
-	
-    $("#co_nuevo2").on("click", function (e) {
-		fn_modal2(1);
-	});
-	$("#co_cerrar_t").on("click", function (e) {
+    
+   $("#co_leer").on("click", fn_Muestra_Filtro);
+    
+   $("#co_cerrar_t").on("click", function (e) {
         window.close(); 
     }); 
  
 //BOTONES ELIMINAR DE LAS GRILLAS
-    $("#co_eliminar").on("click", function (e) {
+    $("#co_eliminar").on("click", function(e){
 		 
         $("#dlg_confirmamod").modal({backdrop: "static",keyboard:false});					
 		$("#dlg_confirmamod").on("shown.bs.modal", function () {
@@ -90,14 +72,13 @@ $(document).ready(function () {
     	
 	});
     
-    $("#co_eliminar2").on("click", function (e) {
+    $("#co_eliminar2").on("click", function(e){
 		 
         $("#dlg_confirmamod2").modal({backdrop: "static",keyboard:false});					
 		$("#dlg_confirmamod2").on("shown.bs.modal", function () {
 			$("#co_confirmamod2_no").focus();
 				});
-    	
-	});
+    });
     
     $("#co_volver_fil").on("click", function (e) {
         
@@ -117,13 +98,9 @@ $(document).ready(function () {
 		$('#div_modal2').modal('hide');
 		fn_limpia_modal2();
 	});
-    
-
-         
-    
 //~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
     
-$("#co_volver2").on("click", function (e) {
+$("#co_volver2").on("click", function (e){
 		$("#div_prin").show();
 		$("#div_tabla").hide();
         $(window).scrollTop(0);
@@ -138,32 +115,31 @@ $("#co_volver2").on("click", function (e) {
     //BOTONES
     
     //BOTONES-FILTRO
-	$("#co_aceptar").on("click", function () {
+	$("#co_aceptar").on("click", function(){
 		if ($("#cb_regional").val() ==""){
 				fn_mensaje_boostrap("CAMPO REGIONAL OBLIGATORIOS", g_tit, $("#cb_regional"));
 				fn_lim_fil_reg();
 				
                 return false;
-                
-			};
+                };
 			
-            if ($("#cb_ciclo").val()==""){
+                if ($("#cb_ciclo").val()==""){
 				fn_mensaje_boostrap("CAMPO CICLO OBLIGATORIO", g_tit, $("#cb_ciclo"));
                  fn_lim_fil_ci();
                  return false;
-			};
+                };
             
-			if ($("#cb_ruta").val()==""){
+			    if ($("#cb_ruta").val()==""){
 				fn_mensaje_boostrap("CAMPO RUTA OBLIGATORIO", g_tit, $("#cb_ruta"));
 				return false;
-			}
+			    }
 			
-            fn_carga_grilla();
-            $('#div_modal').modal('hide');
+                fn_carga_grilla();
+                $('#div_modal').modal('hide');
        
 	});
     
-	$("#co_limpiar").on("click", function () {
+	$("#co_limpiar").on("click", function(){
 		if ($.trim($("#co_limpiar").text()) == "Limpiar") {
 		    fn_limpia_modal();
 			return;
@@ -174,20 +150,16 @@ $("#co_volver2").on("click", function (e) {
 
 //~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*	
      //BOTONES-LECTURA
-    $("#co_aceptar_lec").on("click", function (){
+    $("#co_aceptar_lec").on("click", function(){
 			if ($("#cb_lector").val() == ""){
                     fn_mensaje_boostrap("DIGITE LECTOR", g_tit, $("#cb_lector"));
-				
-				    return false;
-             
-			}	
-			
-		    if (fn_validar_fecha($("#fec_lect").val()) == false) {
+                    return false;
+             }	
+			 if (fn_validar_fecha($("#fec_lect").val()) == false) {
 							fn_mensaje_boostrap("DEBE DILIGENCIAR EL CAMPO FECHA DE PROCESO. EL FORMATO ES DD/MM/YYYY.", g_tit, $("#fec_lect"));
 							return false;
-			}	
-			
-			if ( $("#fec_lect").val() != "" ) {
+			 }	
+			 if ( $("#fec_lect").val() != "" ){
 					
 						var parameters = {
 							"func"       : "fn_validafecha",
@@ -201,7 +173,6 @@ $("#co_volver2").on("click", function (e) {
 
 						HablaServidor(url,parameters,"text", function(text) 
 						{
-						 
 						   if(text == ""){
 							  fn_mensaje_boostrap("NO EXISTEN DATOS DE LECTURA PARA EL CICLO: " + $("#cb_ciclo").val(), g_tit, $(""));
 							  $grid_principal.pqGrid( "refreshDataAndView" );
@@ -304,7 +275,7 @@ $("#co_volver2").on("click", function (e) {
 	
 	//EVENTO DBL_CLICK DE LA GRILLA CICLO - RUTA
     $grid_2.pqGrid({
-		rowDblClick: function( event, ui ) {
+		rowDblClick: function( event, ui ){
 			if (ui.rowData) 
 				{
 					var dataCell = ui.rowData;
@@ -395,7 +366,7 @@ function fn_setea_grid_principal() {
 	$grid_principal = $("#div_grid_principal").pqGrid(obj);
 	$grid_principal.pqGrid("refreshDataAndView");
     
-    /*GRILLA 2*************************************************************/
+//*************************************************************
     
     //Setea grid2
 	data =  [
@@ -433,11 +404,86 @@ function fn_setea_grid_principal() {
         editable:false,
         selectionModel: { type: "row", mode:"single"},
         showTitle:true,
+        filterModel: { mode: 'OR' },
         collapsible:false,
         numberCell: { show: false },
         title: "Detalle",
 		pageModel: { rPP: 200, type: "local", rPPOptions: [100, 200, 500]},
-        scrollModel:{theme:true}
+        scrollModel:{theme:true},
+                 toolbar: {
+                     
+                cls: "pq-toolbar-search",
+                items: [
+                    { type: "<span style='margin:5px;'>Filtro</span>" },
+                    { type: 'textbox', attr: 'placeholder="Ingrese criterio de busqueda"', cls: "filterValue", listeners: [{ 'change': filterhandler}] },
+                    { type: 'select', cls: "filterColumn",
+                        listeners: [{ 'change': filterhandler}],
+                        options: function (ui) {
+                            var CM = ui.colModel;
+                            var opts = [{ '': '[ Buscar en todo]'}];
+                            for (var i = 0; i < CM.length; i++) {
+                                var column = CM[i];
+                                var obj = {};
+                                obj[column.dataIndx] = column.title;
+                                opts.push(obj);
+                            }
+                            return opts;
+                        }
+                    },
+                    { type: 'select', style: "margin:0px 5px;", cls: "filterCondition",
+                        listeners: [{ 'change': filterhandler}],
+                        options: [
+                        { "begin": "Buscar con:" },
+                        { "contain": "Contenido" },
+                        { "end": "Terminado en:" },
+                        { "notcontain": "Que no contenga:" },
+                        { "equal": "Igual a:" },
+                        { "notequal": "Que no sea igual a:" },
+                        { "empty": "Vacio:" },
+                        { "notempty": "No vacio:" },
+                        { "less": "Menos que:" },
+                        { "great": "Mas que:" }    
+                        ]
+                    }
+                ]
+            },
+               toolbar: {
+                     
+                cls: "pq-toolbar-search",
+                items: [
+                    { type: "<span style='margin:5px;'>Filtro</span>" },
+                    { type: 'textbox', attr: 'placeholder="Ingrese criterio de busqueda"', cls: "filterValue", listeners: [{ 'change': filterhandler}] },
+                    { type: 'select', cls: "filterColumn",
+                        listeners: [{ 'change': filterhandler}],
+                        options: function (ui) {
+                            var CM = ui.colModel;
+                            var opts = [{ '': '[ Buscar en todo]'}];
+                            for (var i = 0; i < CM.length; i++) {
+                                var column = CM[i];
+                                var obj = {};
+                                obj[column.dataIndx] = column.title;
+                                opts.push(obj);
+                            }
+                            return opts;
+                        }
+                    },
+                    { type: 'select', style: "margin:0px 5px;", cls: "filterCondition",
+                        listeners: [{ 'change': filterhandler}],
+                        options: [
+                        { "begin": "Buscar con:" },
+                        { "contain": "Contenido" },
+                        { "end": "Terminado en:" },
+                        { "notcontain": "Que no contenga:" },
+                        { "equal": "Igual a:" },
+                        { "notequal": "Que no sea igual a:" },
+                        { "empty": "Vacio:" },
+                        { "notempty": "No vacio:" },
+                        { "less": "Menos que:" },
+                        { "great": "Mas que:" }    
+                        ]
+                    }
+                ]
+            }
       
     };
 	
@@ -453,7 +499,8 @@ function fn_setea_grid_principal() {
     ];
 	
 	obj2.dataModel = { data: data };
-    $grid_2 = $("#div_grid_sec").pqGrid(obj2);
+    $grid_2=$("#div_grid_sec").pqGrid(obj2);
+    $grid_2.pqGrid( "refreshDataAndView" );
 	$grid_2.pqGrid("refreshDataAndView");
 
 }
@@ -482,21 +529,6 @@ function fn_modal(num) {
 
 	});
 }
-
-//~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
-function fn_modal2(num) {
-	
-    $("#tx_nuevo2").val(num);
-    
-    fn_limpia_modal2();
-	
-	$("#div_modal2").modal({ backdrop: "static", keyboard: false });
-	$("#div_modal2").on("shown.bs.modal", function () {
-		$("#div_modal2 div.modal-footer button").focus();
-
-	});
-}
-
 //~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~	
 function fn_limpia_modal() 
 {
@@ -566,7 +598,31 @@ function fn_Muestra_Filtro()
 	$(window).scrollTop(0);
 
 }
+function filterhandler(evt, ui){
 
+            var $toolbar = $grid.find('.pq-toolbar-search'),
+                $value = $toolbar.find(".filterValue"),
+                value = $value.val(),
+                condition = $toolbar.find(".filterCondition").val(),
+                dataIndx = $toolbar.find(".filterColumn").val(),
+                filterObject;
+
+            if (dataIndx == "") {
+                filterObject = [];
+                var CM = $grid.pqGrid("getColModel");
+                for (var i = 0, len = CM.length; i < len; i++) {
+                    var dataIndx = CM[i].dataIndx;
+                    filterObject.push({ dataIndx: dataIndx, condition: condition, value: value });
+                }
+            }
+            else {
+                filterObject = [{ dataIndx: dataIndx, condition: condition, value: value}];
+            }
+            $grid.pqGrid("filter", {
+                oper: 'replace',
+                data: filterObject
+            });
+        }
 
 //*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
 function fn_validar_fecha(value){
