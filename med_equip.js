5a544033304bd42ad813b50718cb3f1760e656ac
Juan David Castrillon Diaz <Juandacasdiaz@gmail.com>
2019-10-09 13:04:22 -0500

    Actulización med_equip

--- med_equip.js
+++ med_equip.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
 	$("#div_footer").load("syn_globales/footer.htm");	
 		
 	$("#tx_cliente").focus();
-   
+	fn_setea_grid_principal();
     fn_marca();
 	fn_diametro();
     fn_tarifa();
@@ -145,6 +145,74 @@ $(document).ready(function() {
 
 
 //*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
+function fn_setea_grid_principal() {
+	
+	var obj = {
+		height: "50%",
+		showTop: true,
+		showBottom: true,
+		showHeader: true,
+		roundCorners: true,
+		rowBorders: true,
+		columnBorders: true,
+		editable: false,
+		editor: { type: "textbox", select: true, style: "outline:none;" },
+		selectionModel: { type: 'cell' },
+		numberCell: { show: true},
+		title: "Tipo de medición",
+		pageModel: { type: "local" },
+		scrollModel: { theme: true },
+		toolbar:
+		{
+			cls: "pq-toolbar-export",
+			items:[
+			
+				{ type: "button", label: "Excel", attr:"id=co_excel", cls:"btn btn-primary btn-sm"},
+                      
+				]
+		},
+        editModel: {
+            clicksToEdit: 1,
+            keyUpDown: true,
+            pressToEdit: true,
+            cellBorderWidth: 0
+        },
+		dataModel:{ data: [
+			{ C1: '0001', C2: 'CONSUMO DE AGUA', C3: '1', C4: 1212121, C5: '1', C6: '1', C7: "1", C8: '1'},
+			{ C1: '0002', C2: 'CONSUMO DE AGUA NO FACTURADO',C3: '2', C4: 455455, C5: '2', C6:'2', C7: "2", C8: '2'},
+			{ C1: '0003', C2: 'SUBSIDIADO POR CASO SOCIAL', C3: '3', C4: 421212, C5: '3', C6: '3', C7: "3", C8: '3'},
+			{ C1: '0004', C2: 'SUBSIDIO POR CASO SOCIAL',C3: '1', C4: 584884, C5: '1', C6:'1', C7: "1", C8: '1'},
+			{ C1: '0005', C2: 'MATERIALES AGUA',C3: '2', C4: 8878, C5: '2', C6:'2', C7: "2", C8: '2'},
+			{ C1: '0006', C2: 'MANO DE OBRA - AGUA ',C3: '3', C4: 847848, C5: '3', C6: '3', C7: "1", C8: '3'},
+			{ C1: '0007', C2: 'CONSUMO DE AGUA HISTORICO', C3: '1', C4: 78777, C5: '1', C6: "1", C7: "2", C8: '1'},
+			{ C1: '0008', C2: 'DERECHO DE CONEXION - AGUA',C3: '2', C4: 878989, C5: '2', C6: "2", C7: "1", C8: '2'},
+			{ C1: '0009', C2: 'REINST. SERVICIO AGUA POTABLE',C3: '3', C4: 811555, C5: '3', C6: "3", C7: "2", C8: '3'},
+			{ C1: '00010', C2: 'DESCUENTO DE EMPLEADO', C3: '1', C4: 646545, C5: '1', C6: "1", C7: "1", C8: '1'},
+			{ C1: '00011', C2: 'DESCUENTO DE JUBILADO',C3: '2', C4: 48441, C5: '2', C6: "1", C7: "3", C8: '2'},
+			{ C1: '00012', C2: 'COMPEM DEFICIENCIA SUMINISTRO AGUA',C3: '3', C4: 485545521, C5: '3', C6:"2", C7: "1", C8: '1'},
+			{ C1: '00013', C2: 'CONSUMO DE AGUA - DITO RELIQ',C3: '1', C4: 54551, C5: '1', C6:"3", C7: "1", C8: '2'},
+			{ C1: '00014', C2: 'CONSUMO DE AGUA - CREDITO RELIQ',C3: '2', C4: 6968568, C5: '2', C6: "1", C7: "2", C8: '3'},
+			{ C1: '00015', C2: 'DEBITO RELIQ DE SUBSIDIOS',C3: '3', C4: 98992, C5: '3', C6: "2", C7: "1", C8: '1'},
+			{ C1: '00016', C2: 'CREDITO RELIQ. JUBILADO/ EMPELADO', C3: '1', C4: 0202020, C5: '1', C6:"3", C7: "2", C8: '2'},
+			{ C1: '00017', C2: 'DEBITO RELIQ. JUBILADO/EMPELADO',C3: '2', C4: 96336966, C5: '2', C6: "1", C7: "3", C8: '3'},
+			{ C1: '00018', C2: 'MANEJO DE CHEQUE DEVUELTO', C3: '3', C4: 1121212, C5: '2', C6: "2", C7: "1", C8: '1'},
+			{ C1: '00019', C2: 'COSTOS LEGALES',C3: '1', C4: 336698, C5: '10000', C6: "3", C7: "1", C8: '2'},
+			{ C1: '00020', C2: 'RECARGO POR PAGO ATRASADO', C3: '2', C4: 343443434, C5: '1', C6: "1", C7: "2", C8: '3'},
+			{ C1: '00021', C2: 'ARREGLO DE PAGO - AGUA',C3: '3', C4:78787878, C5: '10000', C6: "2", C7: "1", C8: '1'},
+			{ C1: '00022', C2: 'RECARGOPAGO ATRASADO - HISTORICO',C3: '1', C4: 89888111, C5: '3', C6: "3", C7: "2", C8: '2'}
+
+        ] }
+	};
+	
+    obj.colModel = [
+		{ title: "Codigo", width:55, dataType: "number", dataIndx: "C1", halign: "center", align: "center" },
+		{ title: "Descripción", width: 300, dataType: "string", dataIndx: "C2", halign: "center", align: "center"},
+        { title: "Constante",width: 300, dataType: "string", dataIndx: "C3", halign: "center", align: "center" },
+	];
+
+	$grid_principal = $("#div_grid_principal").pqGrid(obj);
+	$grid_principal.pqGrid("refreshDataAndView");
+}
 /////////////////////////////////FUNCIONES COMBOS///////////////////////////////////////////
 function fn_marca() {
 
