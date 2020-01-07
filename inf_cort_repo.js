2b8d5373dda847a4d2812116c929e99e9ad68fab
Victoraso26 <32934402+Victoraso26@users.noreply.github.com>
2020-01-07 16:51:22 -0500

    Avances

---
+++ inf_cort_repo.js
@@ -0,0 +1,68 @@
+var g_modulo = "Corte y Reposición";
+var g_tit = "Informes de Corte y Reposición";
+var $grid_principal;
+var sql_grid_prim = "";
+var my_url = "act_tipo_doc";
+var parameters = {};
+
+//~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+$(document).keydown(function (e) {
+
+    if (e.keyCode === 8) {
+        var element = e.target.nodeName.toLowerCase();
+        if ((element != 'input' && element != 'textarea') || $(e.target).attr("readonly")) {
+            return false;
+        }
+    }
+});
+
+//~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+
+$(document).ready(function () {
+    jQuery('#tx_cod').keypress(function (tecla) {
+        if (tecla.charCode < 48 || tecla.charCode > 57) return false;
+    });
+    jQuery('#tx_cons').keypress(function (tecla) {
+        if (tecla.charCode < 48 || tecla.charCode > 57) return false;
+    });
+
+    //COMBOS
+
+
+    // PARA ELIMINAR EL SUBMIT
+    $("button").on("click", function () {
+        return false;
+    });
+    //INGRESA LOS TITULOS
+    document.title = g_tit;
+    document.body.scroll = "yes";
+    ///raiz/
+    $("#div_header").load("syn_globales/header.htm", function () {
+        $("#div_mod0").html(g_modulo);
+        $("#div_tit0").html(g_tit);
+    });
+
+    //Footer  ///raiz/
+    $("#div_footer").load("syn_globales/footer.htm");
+    $("#excel_archivo").val("tablas_generales.xls");
+    $("#tx_empresa").val("1");
+    $("#tx_rol").val("SYNERGIA");
+    $("#tx_ip").val("127.0.0.1");
+
+    //DIBUJA LOS ICONOS DE LOS BOTONES
+
+});
+
+//~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+//BOTONES-EVENTOS
+
+
+//~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
+
+    function fn_mensaje(id,mensaje,segundos)
+    {
+        $(id).show();
+        $(id).html(mensaje);
+        setTimeout(function(){$(id).html("");$(id).hide(); }, segundos);
+    }
+
