acf5503125eba55163bf1e20b55e5a50c308a259
Victoraso26 <32934402+Victoraso26@users.noreply.github.com>
2020-02-20 10:55:13 -0500

    Update

--- gen_ord_cambio_med.js
+++ gen_ord_cambio_med.js
@@ -79,6 +79,24 @@ $(document).ready(function () {
                 return;
             }
 
+            if ($("#cb_sect_inicial").val() !== "" || $("#cb_sect_final").val() !== "") {
+
+                if ($("#cb_sect_inicial").val() === "") {
+                    fn_mensaje_boostrap("FAVOR SELECCIONE UN SECTOR INICIAL", g_tit, $("#cb_sect_inicial"));
+                    return;
+                }
+
+                if ($("#cb_sect_final").val() === "") {
+                    fn_mensaje_boostrap("FAVOR SELECCIONE UN SECTOR FINAL", g_tit, $("#cb_sect_final"));
+                    return;
+                }
+
+                if ($("#cb_sect_inicial option:selected").text() > $("#cb_sect_final option:selected").text()) {
+                    fn_mensaje_boostrap("Rango Selector incorrecto", g_tit, $("#cb_sect_final"));
+                    return;
+                }
+            }
+
             if ($("#tx_zona_inicial").val() !== "" || $("#tx_zona_final").val() !== "") {
 
                 if ($("#tx_zona_inicial").val() === "") {
@@ -683,12 +701,90 @@ function fn_propiedad(){
 
 function fn_sect_inicial(){
 
-    $("#cb_sect_inicial").html("<option value='' selected></option><option value='1'>OPCIÓN 01</option> <option value='2' >OPCIÓN 02</option>  <option value='3' >OPCIÓN 03</option>");
+    $("#cb_sect_inicial").html("<option value='' selected></option><option value='1'>01</option> " +
+        "<option value='2'>02</option>" +
+        "<option value='3'>03</option>" +
+        "<option value='4'>04</option>" +
+        "<option value='5'>05</option>" +
+        "<option value='6'>06</option>" +
+        "<option value='7'>07</option>" +
+        "<option value='8'>08</option>" +
+        "<option value='9'>09</option>" +
+        "<option value='10'>10</option>" +
+        "<option value='11'>11</option>" +
+        "<option value='12'>12</option>" +
+        "<option value='13'>13</option> " +
+        "<option value='14'>14</option>" +
+        "<option value='15'>15</option>" +
+        "<option value='16'>16</option>" +
+        "<option value='17'>17</option>" +
+        "<option value='18'>18</option>" +
+        "<option value='19'>19</option>" +
+        "<option value='20'>20</option>" +
+        "<option value='21'>21</option>" +
+        "<option value='22'>22</option>" +
+        "<option value='23'>23</option>" +
+        "<option value='24'>24</option>" +
+        "<option value='25'>25</option>" +
+        "<option value='26'>26</option>" +
+        "<option value='27'>27</option>" +
+        "<option value='28'>28</option>" +
+        "<option value='29'>29</option>" +
+        "<option value='30'>30</option>" +
+        "<option value='31'>31</option>" +
+        "<option value='32'>32</option>" +
+        "<option value='33'>33</option>" +
+        "<option value='34'>34</option>" +
+        "<option value='35'>35</option>" +
+        "<option value='36'>36</option>" +
+        "<option value='37'>37</option>" +
+        "<option value='38'>38</option>" +
+        "<option value='39'>39</option>" +
+        "<option value='40'>40</option>");
 }
 
 function fn_sect_final(){
 
-    $("#cb_sect_final").html("<option value='' selected></option><option value='1'>OPCIÓN 01</option> <option value='2' >OPCIÓN 02</option>  <option value='3' >OPCIÓN 03</option>");
+    $("#cb_sect_final").html("<option value='' selected></option><option value='1'>01</option> " +
+        "<option value='2'>02</option>" +
+        "<option value='3'>03</option>" +
+        "<option value='4'>04</option>" +
+        "<option value='5'>05</option>" +
+        "<option value='6'>06</option>" +
+        "<option value='7'>07</option>" +
+        "<option value='8'>08</option>" +
+        "<option value='9'>09</option>" +
+        "<option value='10'>10</option>" +
+        "<option value='11'>11</option>" +
+        "<option value='12'>12</option>" +
+        "<option value='13'>13</option> " +
+        "<option value='14'>14</option>" +
+        "<option value='15'>15</option>" +
+        "<option value='16'>16</option>" +
+        "<option value='17'>17</option>" +
+        "<option value='18'>18</option>" +
+        "<option value='19'>19</option>" +
+        "<option value='20'>20</option>" +
+        "<option value='21'>21</option>" +
+        "<option value='22'>22</option>" +
+        "<option value='23'>23</option>" +
+        "<option value='24'>24</option>" +
+        "<option value='25'>25</option>" +
+        "<option value='26'>26</option>" +
+        "<option value='27'>27</option>" +
+        "<option value='28'>28</option>" +
+        "<option value='29'>29</option>" +
+        "<option value='30'>30</option>" +
+        "<option value='31'>31</option>" +
+        "<option value='32'>32</option>" +
+        "<option value='33'>33</option>" +
+        "<option value='34'>34</option>" +
+        "<option value='35'>35</option>" +
+        "<option value='36'>36</option>" +
+        "<option value='37'>37</option>" +
+        "<option value='38'>38</option>" +
+        "<option value='39'>39</option>" +
+        "<option value='40'>40</option>");
 }
 
 //~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
