bd6711419f4d1e6b843ab5c3a66521d7181bc60f
Victor-177 <32934402+Victor-177@users.noreply.github.com>
2020-03-03 16:10:56 -0500

    Update ing_soli_esp_cort.js

--- ing_soli_esp_cort.js
+++ ing_soli_esp_cort.js
@@ -42,6 +42,9 @@ $(document).ready(function () {
     $("#tx_ip").val("127.0.0.1");
 
     $('input[type=radio][name=opt_debe_valid]').filter('[value="opt_2"]').attr('checked', true);
+    fn_act_desact(1);
+
+    $("#co_cancelar").hide();
 
 //~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
 
@@ -51,10 +54,10 @@ $(document).ready(function () {
 
         if ($.trim($("#co_ingresar").text() === "Ingresar")) {
 
-            /*if ($("#input_file").val() === "") {
+            if ($("#input_file").val() === "") {
                 fn_mensaje_boostrap("Error, por favor cargue un archivo.", g_tit, $("#input_file"));
                 return;
-            }*/
+            }
 
             if ($("#cb_inst_cort").val() === "") {
                 fn_mensaje_boostrap("Error, por favor seleccione una Instancia de Corte.", g_tit, $("#cb_inst_cort"));
@@ -78,8 +81,22 @@ $(document).ready(function () {
                 }
             }
 
+            if ($("#tx_fech_prog_cort").val() !== "") {
+
+                if (fn_fecha($("#tx_fech_prog_cort").val()) === false) {
+                    fn_mensaje_boostrap("Error, la fecha ingresada no sebe ser inferior a la fecha actual.", g_tit, $("#tx_fech_prog_cort"));
+                    $("#tx_fech_prog_cort").focus();
+                    return;
+                }
+            }
+
             if ($("#tx_observ").val() === "") {
-                fn_mensaje_boostrap("Error, por favor inidque una Observación.", g_tit, $("#tx_observ"));
+                fn_mensaje_boostrap("Error, por favor indique una Observación.", g_tit, $("#tx_observ"));
+                return;
+            }
+
+            if ($("#tx_observ").val().length <= 15) {
+                fn_mensaje_boostrap("Error, la Observación debe ser mas larga.", g_tit, $("#tx_observ"));
                 return;
             }
 
@@ -90,12 +107,25 @@ $(document).ready(function () {
 
     $("#input_file").change( function () {
 
-        if($.inArray($('#input_file').val().split('.').pop().toLowerCase(), ['txt']) === -1) {
+        if ($.inArray($('#input_file').val().split('.').pop().toLowerCase(), ['txt']) === -1) {
             if ($("#input_file").val() !== "") {
-                fn_mensaje_boostrap("Error, solo se permiten archivos con extensión .txt", g_tit, $("#input_file"));
+                fn_mensaje_boostrap("Error, solo se admiten archivos con extensión .txt", g_tit, $("#input_file"));
                 $("#input_file").val("");
             }
         }
+
+        if ($("#input_file").val() !== "") {
+            fn_leer_archivo();
+            fn_act_desact(2);
+        } else {
+            fn_act_desact(1)
+        }
+    });
+
+    $("#co_cancelar").on("click", function () {
+
+        fn_limpiar();
+        fn_act_desact(1);
     });
 
     $("#co_cerrar").on("click", function (){ window.close(); });
@@ -120,6 +150,81 @@ function fn_motivo(){
 
 //                                  <-- Functions -->
 
+function fn_leer_archivo() {
+    var archivo = document.getElementById('input_file').files[0];
+    if (!archivo) {
+        return;
+    }
+    var lector = new FileReader();
+    lector.onload = function(e) {
+        var contenido = e.target.result;
+        fn_validar_archivo(contenido);
+    };
+
+    lector.readAsText(archivo);
+}
+
+function fn_validar_archivo(contenido) {
+
+    var codigos = contenido.split("|");
+
+    if (contenido === "") {
+        fn_mensaje_boostrap("Error, el archivo cargado esta vacio.", g_tit, $("#input_file"));
+
+        fn_act_desact(1);
+        fn_limpiar();
+        return;
+    }
+
+    if (codigos.length > 1) {
+        if (isNaN(codigos[0])) {
+            fn_mensaje_boostrap("Error, el archivo cargado no cumple con los valores requeridos.", g_tit, $("#input_file"));
+
+            fn_act_desact(1);
+            fn_limpiar();
+            return;
+        }
+
+        for (var i = 0; i <= codigos.length; i++) {
+
+            if (codigos[i].length > 6) {
+                fn_mensaje_boostrap("Error, en el archivo cargado se encuentra un valor que no cumple con lo requerido.", g_tit, $("#input_file"));
+
+                fn_act_desact(1);
+                fn_limpiar();
+                return;
+            }
+
+            if (isNaN(codigos[i])) {
+                fn_mensaje_boostrap("Error, en el archivo cargado se encuentra un valor que no cumple con lo requerido.", g_tit, $("#input_file"));
+
+                fn_act_desact(1);
+                fn_limpiar();
+                return;
+            }
+        }
+
+    } else {
+        if (contenido.length > 6) {
+            fn_mensaje_boostrap("Error, el archivo cargado no cumple con los valores requeridos.", g_tit, $("#input_file"));
+
+            fn_act_desact(1);
+            fn_limpiar();
+            return;
+        }
+
+        if (isNaN(contenido)) {
+            fn_mensaje_boostrap("Error, el archivo cargado no cumple con los valores requeridos.", g_tit, $("#input_file"));
+
+            fn_act_desact(1);
+            fn_limpiar();
+            return;
+        }
+    }
+
+    console.log("DEBUD: Todo ok")
+}
+
 function fn_validar_fecha(value){
     var real, info;
     if (/^(0?[1-9]|[12][0-9]|3[01])[\/](0?[1-9]|1[012])[\/]\d{4}$/.test(value)) {
@@ -140,7 +245,61 @@ function fn_validar_fecha(value){
     }
 }
 
+function fn_fecha(valor) {
+
+    var fecha = valor.split("/");
+    fecha.reverse();
+
+    var fechaHoy = new Date();
+    var fechaIngresada = new Date(fecha);
+
+    fechaHoy.setHours(0,0,0,0);
+
+    return fechaHoy <= fechaIngresada
+
+}
+
+function fn_act_desact(accion) {
+
+    if (accion === 1) {
+
+        fn_limpiar();
+
+        $("#cb_inst_cort").prop("disabled", true);
+        $("#cb_motivo").prop("disabled", true);
+        $("#rad_opt_valid_si").prop("disabled", true);
+        $("#rad_opt_valid_no").prop("disabled", true);
+        $("#tx_fech_prog_cort").prop("disabled", true);
+        $("#tx_observ").prop("disabled", true);
+
+        $("#co_ingresar").prop("disabled", true);
+        $("#co_cancelar").hide();
+        $("#co_cerrar").show();
+    }
+
+    if (accion === 2) {
+        $("#cb_inst_cort").prop("disabled", false);
+        $("#cb_motivo").prop("disabled", false);
+        $("#rad_opt_valid_si").prop("disabled", false);
+        $("#rad_opt_valid_no").prop("disabled", false);
+        $("#tx_fech_prog_cort").prop("disabled", false);
+        $("#tx_observ").prop("disabled", false);
+
+        $("#co_ingresar").prop("disabled", false);
+        $("#co_cancelar").show();
+        $("#co_cerrar").hide();
+    }
+}
+
 function fn_limpiar() {
+
+    $("#input_file").val("");
+
+    $("#cb_inst_cort").val("");
+    $("#cb_motivo").val("");
+    $('input[type=radio][name=opt_debe_valid]').filter('[value="opt_2"]').attr('checked', true);
+    $("#tx_fech_prog_cort").val("");
+    $("#tx_observ").val("");
 
 }
 
