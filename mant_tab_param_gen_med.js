f530bd8ad74ee0df121072b59ec6557074080bec
Victor-177 <32934402+Victor-177@users.noreply.github.com>
2020-03-19 09:13:38 -0500

    Update mant_tab_param_gen_med

--- mant_tab_param_gen_med.js
+++ mant_tab_param_gen_med.js
@@ -3,7 +3,15 @@ var g_tit = "Mantención de Tablas de Parametros Generales de Medidores";
 var $grid_motiv_camb;
 var $grid_situa_encon;
 var $grid_acc_rea;
+var $grid_relacion;
+var $grid_motiv_camb_2;
+var $grid_situa_encon_2;
+var $grid_acc_rea_2;
+var grid_motiv_camb_2;
+var grid_situa_encon_2;
+var grid_acc_rea_2;
 var rowIndxG;
+var load;
 
 //~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
 $(document).keydown(function (e) {
@@ -33,6 +41,8 @@ $(document).ready(function () {
     fn_set_grid_motiv_camb();
     fn_set_grid_situa_econ();
     fn_set_grid_acc_rea();
+    fn_set_grid_relacion();
+    fn_set_grid_complementarias();
 
     // PARA ELIMINAR EL SUBMIT
     $("button").on("click", function () {
@@ -56,13 +66,16 @@ $(document).ready(function () {
 
     fn_hide();
     $("#div_motiv_camb").show();
-    $("#row_radios_situa").hide();
+    $("#co_leer").hide();
+    $("#div_second").hide();
 
 //~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
 
 //                              <-- Buttons - Listeners -->
 
     $(".nav-tabs a").click(function(){
+
+        $("#co_leer").hide();
 
         $(this).tab('show');
         fn_hide();
@@ -84,6 +97,35 @@ $(document).ready(function () {
 
         if (this.text === "Relación") {
             $("#div_relacion").show();
+
+            $("#co_leer").show();
+        }
+
+    });
+
+    $("#co_aceptar").on("click", function () {
+
+        if ($.trim($("#co_aceptar").text()) === "Aceptar") {
+
+            if (grid_motiv_camb_2.Checkbox('checkBox').getCheckedNodes().length < 1) {
+                fn_mensaje_boostrap("Error, por favor selecciona un Motivo Cambio.", g_tit, $("#div_grid_cargo"));
+                return;
+            }
+
+            if (grid_situa_encon_2.Checkbox('checkBox').getCheckedNodes().length < 1) {
+                fn_mensaje_boostrap("Error, por favor selecciona una Situación Encontrada.", g_tit, $("#div_grid_cod_valor"));
+                return;
+            }
+
+            if (grid_acc_rea_2.Checkbox('checkBox').getCheckedNodes().length < 1) {
+                fn_mensaje_boostrap("Error, por favor selecciona una Acción Realizada.", g_tit, $("#div_grid_cod_valor"));
+                return;
+            }
+
+            $("#div_second").slideUp();
+            $("#div_second").hide();
+            $("#div_prin").show();
+            $("#div_prin").slideDown();
         }
 
     });
@@ -141,6 +183,42 @@ $(document).ready(function () {
                 }
             }
 
+            if ($("#div_relacion").is(":visible")) {
+                if (!$('input[type=radio][name=opt_arch_palm]').is(":checked")) {
+                    fn_mensaje('#mensaje_edit','<div class="alert alert-danger" style="text-align:left;font-size:12px;margin-bottom: 0;" role="alert"><strong>Error, por favor indique si cuenta con Archivos Palm (Si o No).</strong></div>',3000);
+                    $("#opt_arch_palm_si").focus();
+                    return;
+                }
+
+                if (!$('input[type=radio][name=opt_genera_carta]').is(":checked")) {
+                    fn_mensaje('#mensaje_edit','<div class="alert alert-danger" style="text-align:left;font-size:12px;margin-bottom: 0;" role="alert"><strong>Error, por favor indique si Genera Cartera (Si o No).</strong></div>',3000);
+                    $("#opt_genera_carta_si").focus();
+                    return;
+                }
+
+                if (!$('input[type=radio][name=opt_camb_correct]').is(":checked")) {
+                    fn_mensaje('#mensaje_edit','<div class="alert alert-danger" style="text-align:left;font-size:12px;margin-bottom: 0;" role="alert"><strong>Error, por favor indique si cuenta con Cambio Correctivo (Si o No).</strong></div>',3000);
+                    $("#opt_camb_correct_si").focus();
+                    return;
+                }
+
+                if (!$('input[type=radio][name=opt_indq_ord_improc]').is(":checked")) {
+                    fn_mensaje('#mensaje_edit','<div class="alert alert-danger" style="text-align:left;font-size:12px;margin-bottom: 0;" role="alert"><strong>Error, por favor indique si Indica orden Inprocedente (Si o No).</strong></div>',3000);
+                    $("#opt_indq_ord_improc_si").focus();
+                    return;
+                }
+
+                if (grid_cargo.Checkbox('checkBox').getCheckedNodes().length < 1) {
+                    fn_mensaje_boostrap("Error, por favor selecciona un Cargo.", g_tit, $("#div_grid_cargo"));
+                    return;
+                }
+
+                if (grid_codigo_val.Checkbox('checkBox').getCheckedNodes().length < 1) {
+                    fn_mensaje_boostrap("Error, por favor selecciona un Codigo Valor.", g_tit, $("#div_grid_cod_valor"));
+                    return;
+                }
+            }
+
             fn_mensaje_boostrap("Se Modifico", g_tit, $("#co_modificar"));
             $('#div_edit_bts').modal('hide');
             $(window).scrollTop(0);
@@ -152,6 +230,29 @@ $(document).ready(function () {
 
     $("#co_cancel").on("click", function () {
         $('#div_edit_bts').modal('hide');
+    });
+
+    $("#co_leer").on("click", function () {
+
+        $("#div_prin").slideUp();
+        $("#div_prin").hide();
+        $("#div_second").show();
+        $("#div_second").slideDown();
+
+        load = true;
+
+        $grid_motiv_camb_2.pqGrid("refreshView");
+        $grid_situa_encon_2.pqGrid("refreshView");
+        $grid_acc_rea_2.pqGrid("refreshView");
+
+        load = false;
+    });
+
+    $("#co_cancelar").on("click", function () {
+        $("#div_second").slideUp();
+        $("#div_second").hide();
+        $("#div_prin").show();
+        $("#div_prin").slideDown();
     });
 
     $("#co_confirm_yes").on( "click", function () {
@@ -211,6 +312,40 @@ $(document).ready(function () {
                 var dataCell = ui.rowData;
 
                 fn_edit(dataCell, 3);
+            }
+        }
+    });
+
+    $grid_relacion.pqGrid({
+        rowDblClick: function( event, ui ) {
+            if (ui.rowData) {
+                var dataCell = ui.rowData;
+
+                fn_edit(dataCell, 4);
+            }
+        }
+    });
+
+    $grid_motiv_camb_2.pqGrid({
+        refresh: function ( event, ui ) {
+            if (load) {
+                grid_motiv_camb_2 = this;
+            }
+        }
+    });
+
+    $grid_situa_encon_2.pqGrid({
+        refresh: function ( event, ui ) {
+            if (load) {
+                grid_situa_encon_2 = this;
+            }
+        }
+    });
+
+    $grid_acc_rea_2.pqGrid({
+        refresh: function ( event, ui ) {
+            if (load) {
+                grid_acc_rea_2 = this;
             }
         }
     });
@@ -425,7 +560,7 @@ function fn_set_grid_situa_econ() {
 
 function fn_set_grid_acc_rea() {
 
-    // GRID ACCIÓN  REALIZADA
+    // GRID ACCIÓN REALIZADA
 
     var data =  [
         { C1: 'IM', C2: 'INSTALACIÓN DE MEDIDOR', C3: 'NO', C4: '', C5: '28/07/2008', C6: '22/07/2012', C7: 'ACTIVO' },
@@ -493,6 +628,223 @@ function fn_set_grid_acc_rea() {
     $grid_acc_rea = $("#div_grid_acc_rea").pqGrid(obj);
 }
 
+function fn_set_grid_relacion() {
+
+    // GRID RELACIÓN
+
+    var data =  [
+        { C1: '', C2: '', C3: '', C4: '', C5: '', C6: '' },
+    ];
+
+    var obj = {
+        height: "100%",
+        showtop: true,
+        filterModel: {
+            on: true,
+            mode: "AND",
+            header: true,
+        },
+        editable: false,
+        rowBorders: true,
+        showTitle: false,
+        showBottom: true,
+        collapsible: true,
+        roundCorners: true,
+        columnBorders: true,
+        numberCell: { show: true },
+        scrollModel: { theme: true },
+        selectionModel: {type: 'row', mode: 'block'},
+        pageModel: {rPP: 50, type: "local", rPPOptions: [50, 100, 200]},
+        toolbar: false,
+    };
+
+    obj.colModel = [
+        { title: "Código", width: 58, dataType: "string", dataIndx: "C1", halign: "center", align: "center" },
+        { title: "Descripción Motivo", width: 300, dataType: "strig", dataIndx: "C2", halign: "center", align: "left", filter: { crules: [{ condition: 'contain' }] } },
+        { title: "Código", width: 58, dataType: "strig", dataIndx: "C3", halign: "center", align: "center" },
+        { title: "Descripción Sitaución", width: 300, dataType: "strig", dataIndx: "C4", halign: "center", align: "left", filter: { crules: [{ condition: 'contain' }] } },
+        { title: "Código", width: 59, dataType: "strig", dataIndx: "C5", halign: "center", align: "center" },
+        { title: "Descripción Acción", width: 300, dataType: "strig", dataIndx: "C6", halign: "center", align: "left", filter: { crules: [{ condition: 'contain' }] } },
+    ];
+
+    obj.dataModel = { data: data };
+
+    $grid_relacion = $("#div_grid_relacion").pqGrid(obj);
+
+}
+
+function fn_set_grid_complementarias() {
+
+    // GRID COMPLEMENTARIA MOTIVO CAMBIO
+
+    var data =  [
+        { C1: 'RKK', C2: 'R-PRUEBA DE MEDIDOR'},
+        { C1: 'RKA', C2: 'R-MEDIDOR EN MAL ESTADO'},
+        { C1: 'IA1', C2: 'I-SOLICITUD CLIENTE'},
+        { C1: 'IA2', C2: 'I-SOLICITUD FACTURACIÓN'},
+        { C1: 'IA3', C2: 'I-SOLICITUD IDAAN'},
+        { C1: 'IA4', C2: 'I-SIN MOTIVO'},
+        { C1: 'IA5', C2: 'I-INSTALACIÓN MASIVA'},
+        { C1: 'IA6', C2: 'I-SOLICITUD GTE - MEDIDORES'},
+        { C1: 'R3', C2: 'R-MEDIDOR DIFICIL ACCESO'},
+        { C1: 'C9', C2: 'C-MEDIDOR DIFICIL ACCESO'},
+        { C1: 'C4', C2: 'C-VARIACIÓN DE CONSUMO'},
+        { C1: 'C8', C2: 'C-CAMBIO MASIVO'},
+        { C1: 'CJ', C2: 'C-MEDIDOR EN MAL ESTADO'},
+        { C1: 'CT', C2: 'C-MEDIDOR MANIPULADO'},
+        { C1: 'CV', C2: 'C-PRUEBA DE MEDIDOR'},
+        { C1: 'CW', C2: 'C-MEDIDOR PARADO'},
+    ];
+
+    var obj = {
+        height: "100%",
+        showtop: true,
+        filterModel: {
+            on: true,
+            mode: "AND",
+            header: true,
+        },
+        rowBorders: true,
+        showTitle: true,
+        showBottom: false,
+        collapsible: true,
+        roundCorners: true,
+        columnBorders: true,
+        title: "Motivo Cambio",
+        postRenderInterval: 0,
+        numberCell: { show: true },
+        scrollModel: { theme: true },
+        selectionModel: {type: 'row', mode: 'block'},
+        pageModel: {rPP: 500, type: "local", rPPOptions: [500, 1000, 2000]},
+        toolbar: false,
+    };
+
+    obj.colModel = [
+        { dataIndx: "checkBox", maxWidth: 30, minWidth: 30, align: "center", resizable: false, title: "", dataType: 'bool', editable: true,
+            type: 'checkBoxSelection', cls: 'ui-state-default', sortable: false, editor: true,
+            cb: {
+                maxCheck: 1,
+                select: true
+            }
+        },
+        { title: "Código", width: 80, dataType: "string", dataIndx: "C1", halign: "center", align: "center", editable: false },
+        { title: "Descripción", width: 965, dataType: "strig", dataIndx: "C2", halign: "center", align: "left", editable: false, filter: { crules: [{ condition: 'contain' }] } },
+    ];
+
+    obj.dataModel = { data: data };
+
+    $grid_motiv_camb_2 = $("#div_grid_motiv_camb_2").pqGrid(obj);
+
+    // GRID COMPLEMENTARIA SITUACION ENCONTRADA
+
+    var data2 =  [
+        { C1: 'AA', C2: 'ARRANQUE SIN MEDIDOR'},
+        { C1: 'AB', C2: 'MEDIDOR INVERTIDO'},
+        { C1: 'AC', C2: 'AJUSTE DATOS (CAMBIO)'},
+        { C1: 'AG', C2: 'ARRANQUE GALVANIZADO'},
+        { C1: 'AI', C2: 'AJUSTE DATOS (INSTALACIÓN)'},
+        { C1: 'AI', C2: 'ARRANQUE INTERVENIDO'},
+        { C1: 'AR', C2: 'AJUSTE DATOS (RETIRO)'},
+        { C1: 'C', C2: 'ORDEN CAMBIO EJECUTADA'},
+        { C1: 'CD', C2: 'CIFRAS VARIADAS (CAMBIADO)'},
+        { C1: 'CO', C2: 'ATENCIÓN OPERACIONES (CAMBIADO)'},
+        { C1: 'I', C2: 'ORDEN INTALACIÓN EJECUTADA'},
+        { C1: 'M3', C2: 'ACTUALIZACIÓN CATASTRO'},
+        { C1: 'N1', C2: 'MEDIDOR AJUSTADO EN TERRENO'},
+        { C1: 'NE', C2: 'SEÓS MANIPULACIÓN (NORMALIZADO SELLADO)'},
+        { C1: 'NM', C2: 'LECTURA INFERIOR SIN MANIPULACIÓN (NORMALIZADO)'},
+        { C1: 'R', C2: 'ORDEN RETIRO EJECUTADA'},
+    ];
+
+    var obj2 = {
+        height: "100%",
+        showtop: true,
+        filterModel: {
+            on: true,
+            mode: "AND",
+            header: true,
+        },
+        rowBorders: true,
+        showTitle: true,
+        showBottom: false,
+        collapsible: true,
+        roundCorners: true,
+        columnBorders: true,
+        postRenderInterval: 0,
+        numberCell: { show: true },
+        scrollModel: { theme: true },
+        title: "Situación Encontrada",
+        selectionModel: {type: 'row', mode: 'block'},
+        pageModel: {rPP: 500, type: "local", rPPOptions: [500, 1000, 2000]},
+        toolbar: false,
+    };
+
+    obj2.colModel = [
+        { dataIndx: "checkBox", maxWidth: 30, minWidth: 30, align: "center", resizable: false, title: "", dataType: 'bool', editable: true,
+            type: 'checkBoxSelection', cls: 'ui-state-default', sortable: false, editor: true,
+            cb: {
+                maxCheck: 1,
+                select: true
+            }
+        },
+        { title: "Código", width: 80, dataType: "string", dataIndx: "C1", halign: "center", align: "center", editable: false },
+        { title: "Descripción", width: 965, dataType: "strig", dataIndx: "C2", halign: "center", align: "left", editable: false, filter: { crules: [{ condition: 'contain' }] } },
+    ];
+
+    obj2.dataModel = { data: data2 };
+
+    $grid_situa_encon_2 = $("#div_grid_situa_econ_2").pqGrid(obj2);
+
+    // GRID COMPLEMENTARIA ACCIÓN REALZADA
+
+    var data3 =  [
+        { C1: 'CM', C2: 'CAMBIO DE MEDIDOR'},
+        { C1: 'CN', C2: 'OCM NO EJECUTADA (IMPROCEDENTE)'},
+        { C1: 'IM', C2: 'INSTALACIÓN DE MEDIDOR'},
+        { C1: 'MA', C2: 'MEDIDOR NORMALIZADO'},
+        { C1: 'RM', C2: 'RETIRO DE MEDIDOR'},
+    ];
+
+    var obj3 = {
+        height: "100%",
+        showtop: true,
+        filterModel: {
+            on: true,
+            mode: "AND",
+            header: true,
+        },
+        rowBorders: true,
+        showTitle: true,
+        showBottom: false,
+        collapsible: true,
+        roundCorners: true,
+        columnBorders: true,
+        postRenderInterval: 0,
+        title: "Acción Realizada",
+        numberCell: { show: true },
+        scrollModel: { theme: true },
+        selectionModel: {type: 'row', mode: 'block'},
+        pageModel: {rPP: 500, type: "local", rPPOptions: [500, 1000, 2000]},
+        toolbar: false,
+    };
+
+    obj3.colModel = [
+        { dataIndx: "checkBox", maxWidth: 30, minWidth: 30, align: "center", resizable: false, title: "", dataType: 'bool', editable: true,
+            type: 'checkBoxSelection', cls: 'ui-state-default', sortable: false, editor: true,
+            cb: {
+                maxCheck: 1,
+                select: true
+            }
+        },
+        { title: "Código", width: 80, dataType: "string", dataIndx: "C1", halign: "center", align: "center", editable: false },
+        { title: "Descripción", width: 965, dataType: "strig", dataIndx: "C2", halign: "center", align: "left", editable: false, filter: { crules: [{ condition: 'contain' }] } },
+    ];
+
+    obj3.dataModel = { data: data3 };
+
+    $grid_acc_rea_2 = $("#div_grid_acc_rea_2").pqGrid(obj3);
+}
+
 //~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
 
 //                                  <-- Combos -->
@@ -520,6 +872,14 @@ function fn_acc_rea() {
 //                                  <-- Functions -->
 
 function fn_edit(dataCell, grid) {
+
+    fn_limpiar();
+
+    $("#row1").show();
+    $("#space1").show();
+    $("#space2").show();
+    $("#div_fechas").show();
+    $("#row_radios_situa").hide();
 
     if (grid === 1) {
 
@@ -634,6 +994,24 @@ function fn_edit(dataCell, grid) {
 
     }
 
+    if (grid === 4) {
+
+        $("#div_fechas").hide();
+        $("#row1").hide();
+        $("#space1").hide();
+        $("#space2").hide();
+
+        $("#row2_pt_i").html("<div class=\"row\"> <div class=\"col-xs-4\"> <label for=\"cb_acc_rea\">Acción:</label> </div> <div class=\"col-xs-8\"> <select class=\"form-control\" id=\"cb_acc_rea\" name=\"cb_acc_rea\"></select> </div> </div>");
+        $("#row2_pt_ii").html("");
+        $("#row3_pt_i").html("<div class=\"row\"> <div class=\"col-xs-4\"> <div class=\"row\"> <div class=\"col-xs-12\"> <label>Archivos Palm:</label> </div> </div> </div> <div class=\"col-xs-6\"> <div class=\"row\"> <div class=\"col-xs-4\"><span class=\"radio-inline\"><input class=\"radio-inline\" type=\"radio\" id=\"opt_arch_palm_si\" name=\"opt_arch_palm\" value=\"opt_1\"><label for=\"opt_arch_palm_si\">Si</label></span> </div> <div class=\"col-xs-4\"> <span class=\"radio-inline\"><input class=\"radio-inline\" type=\"radio\" id=\"opt_arch_palm_no\" name=\"opt_arch_palm\" value=\"opt_2\"><label for=\"opt_arch_palm_no\">No</label></span> </div> </div> </div> </div>");
+        $("#row3_pt_ii").html("<div class=\"row\"> <div class=\"col-xs-6\"> <div class=\"row\"> <div class=\"col-xs-12\"> <label>Indica orden Improcedente:</label> </div> </div> </div> <div class=\"col-xs-6\"> <div class=\"row\"> <div class=\"col-xs-4\"><span class=\"radio-inline\"><input class=\"radio-inline\" type=\"radio\" id=\"opt_indq_ord_improc_si\" name=\"opt_indq_ord_improc\" value=\"opt_1\"><label for=\"opt_indq_ord_improc_si\">Si</label></span> </div> <div class=\"col-xs-4\"> <span class=\"radio-inline\"><input class=\"radio-inline\" type=\"radio\" id=\"opt_indq_ord_improc_no\" name=\"opt_indq_ord_improc\" value=\"opt_2\"><label for=\"opt_indq_ord_improc_no\">No</label></span> </div> </div> </div> </div>");
+
+        $("#row_radios_situa").show();
+
+        fn_acc_rea();
+
+    }
+
     $("#div_edit_bts").modal({backdrop: "static",keyboard:false});
     $("#div_edit_bts").on("shown.bs.modal", function () {
         $("#div_edit_bts div.modal-footer button").focus();
@@ -687,6 +1065,13 @@ function fn_limpiar() {
 
     if ($("#div_acc_rea").is(":visible")) {
 
+        $('input[type=radio][name=opt_indq_ord_improc]').prop("checked", false);
+    }
+
+    if ($("#div_relacion").is(":visible")) {
+        $('input[type=radio][name=opt_arch_palm]').prop("checked", false);
+        $('input[type=radio][name=opt_genera_carta]').prop("checked", false);
+        $('input[type=radio][name=opt_camb_correct]').prop("checked", false);
         $('input[type=radio][name=opt_indq_ord_improc]').prop("checked", false);
     }
 
